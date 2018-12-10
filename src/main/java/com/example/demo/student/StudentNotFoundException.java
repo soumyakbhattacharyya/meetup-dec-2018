4692cfb2b979399009c7b4bb4f6ddbd793f4df92
soumyak.bhattacharyya <bhattacharyya.soumyak@gmail.com>
2018-12-10 17:41:58 +0530

    initial commit

---
+++ src/main/java/com/example/demo/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.demo.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+  public StudentNotFoundException(String exception) {
+    super(exception);
+  }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into student
+values(10001,'Tom', 'E1234567');
+
+insert into student
+values(10002,'Jerry', 'A1234568');
